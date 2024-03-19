04bc79b8d453d95b425df24aa229049669687135
CHAY WILKINSON MARKEY <chay@infohost.net.au>
2024-03-19 14:56:56 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,22 +55,12 @@ _isKVM() {
     fi
 }
 
-# Install Yay
-_installYay() {
-    if sudo pacman -Qs yay > /dev/null ; then
-        echo "yay is already installed!"
-    else
-        echo "yay is not installed. Will be installed now!"
-        _installPackagesPacman "base-devel"
-        SCRIPT=$(realpath "$0")
-        temp_path=$(dirname "$SCRIPT")
-        echo $temp_path
-        git clone https://aur.archlinux.org/yay-git.git ~/yay-git
-        cd ~/yay-git
-        makepkg -si
-        cd $temp_path
-        echo "yay has been installed successfully."
-    fi
+# Install Paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
 }
 
 # Required packages for the installer
