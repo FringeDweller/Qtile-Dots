d43c94a3aa1d8ffc1eb344104480955606797484
Chay <chay@infohost.net.au>
2024-03-22 09:47:53 +1000

    Update packages.py

--- scripts/packages.py
+++ scripts/packages.py
@@ -1,53 +1,58 @@
 import subprocess
 import os
 import shutil
+import pwd
 
-def check_ssh():
-    print("Checking if OpenSSH is installed...")
-    installed = subprocess.run(['sudo', 'pacman', '-Q', 'openssh'], capture_output=True)
-    if installed.returncode != 0:
-        print("OpenSSH is not installed. Installing and enabling...")
-        subprocess.run(['sudo', 'pacman', '-Sy', 'openssh'])
-    print("Enabling and starting SSH service...")
-    subprocess.run(['sudo', 'systemctl', 'enable', 'sshd'])
-    subprocess.run(['sudo', 'systemctl', 'start', 'sshd'])
-    
 def check_packages():
-    print("Checking required packages...")
+    print("Checking and installing required packages with pacman...")
     required_packages = [
         "git", "xorg", "xorg-xinit", "nitrogen", "thunar", "picom", 
         "rofi", "alacritty", "dunst", "neofetch", "ttf-font-awesome", 
         "zsh", "qemu-full", "virt-manager", "virt-viewer", "dnsmasq", 
         "bridge-utils", "libguestfs", "ebtables", "vde2", "openbsd-netcat", 
-        "mesa", "neovim", "geany", "geany-plugins"
+        "mesa", "neovim", "geany", "geany-plugins", "openssh"
     ]
     for package in required_packages:
         installed = subprocess.run(['sudo', 'pacman', '-Q', package], capture_output=True)
         if installed.returncode != 0:
             print(f"{package} is not installed. Installing...")
-            subprocess.run(['sudo', 'pacman', '-Sy', package])
-        else:
-            print(f"{package} is already installed.")
+            subprocess.run(['sudo', 'pacman', '-Sy', '--noconfirm', package])
 
 def check_yay():
-    print("Checking if Yay is installed...")
+    print("Checking and installing Yay...")
     installed = subprocess.run(['sudo', 'pacman', '-Q', 'yay'], capture_output=True)
     if installed.returncode != 0:
         print("Yay is not installed. Installing using git...")
         subprocess.run(['git', 'clone', 'https://aur.archlinux.org/yay.git'])
         os.chdir('yay')  # Change the working directory to 'yay'
-        subprocess.run(['makepkg', '-si'])
+        subprocess.run(['makepkg', '-si', '--noconfirm'])
         os.chdir('..')  # Change back to the parent directory
         shutil.rmtree('yay')  # Remove the yay folder
     else:
         print("Yay is already installed.")
 
-def clone_dots():
-    print("Cloning FringeDweller/dots repository to /home...")
-    if not os.path.exists('/home/dots'):
-        subprocess.run(['git', 'clone', 'https://github.com/FringeDweller/dots.git', '/home/dots'])
-    else:
-        print("dots repository already exists in /home.")
+def check_optional_packages():
+    print("Checking and installing optional packages with Yay...")
+    optional_packages = ["xrdp", "xorgxrdp", "octopi", "microsoft-edge-stable-bin", "code-git"]
+    for package in optional_packages:
+        installed = subprocess.run(['sudo', 'pacman', '-Q', package], capture_output=True)
+        if installed.returncode != 0:
+            print(f"{package} is not installed. Installing using Yay...")
+            subprocess.run(['yay', '-Sy', '--noconfirm', package])
+
+def check_ssh():
+    print("Checking and enabling SSH service...")
+    subprocess.run(['sudo', 'systemctl', 'enable', 'sshd'])
+    subprocess.run(['sudo', 'systemctl', 'start', 'sshd'])
+
+def check_xrdp():
+    print("Checking and enabling XRDP service...")
+    subprocess.run(['sudo', 'systemctl', 'enable', 'xrdp'])
+    subprocess.run(['sudo', 'systemctl', 'start', 'xrdp'])
+
+def set_zsh_default_shell():
+    print("Setting Zsh as the default shell...")
+    subprocess.run(['chsh', '-s', '/bin/zsh', pwd.getpwuid(os.getuid()).pw_name])
 
 def create_symlinks():
     print("Creating symbolic links between /home/dots directories and ~/.config...")
@@ -63,30 +68,15 @@ def create_symlinks():
             else:
                 print(f"Symlink already exists: {source_dir} -> {target_dir}")
 
-def set_default_shell():
-    print("Setting zsh as default shell...")
-    subprocess.run(['sudo', 'chsh', '-s', '/bin/zsh'])
-
-def check_optional_packages():
-    print("Checking optional packages...")
-    optional_packages = ["xrdp", "xorgxrdp", "octopi", "microsoft-edge-stable-bin"]
-    for package in optional_packages:
-        installed = subprocess.run(['sudo', 'pacman', '-Q', package], capture_output=True)
-        if installed.returncode != 0:
-            print(f"{package} is not installed. Installing using Yay...")
-            subprocess.run(['yay', package])
-        else:
-            print(f"{package} is already installed.")
-
 def main():
     print("Starting system configuration...")
-    check_ssh()
     check_packages()
     check_yay()
-    clone_dots()
-    create_symlinks()
-    set_default_shell()
     check_optional_packages()
+    check_ssh()
+    check_xrdp()
+    set_zsh_default_shell()
+    create_symlinks()
     print("System configuration completed.")
 
 if __name__ == "__main__":
