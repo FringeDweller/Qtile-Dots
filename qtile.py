7b87e90b49f597a635e2c643f584098f71ef74ac
Chay <chay@infohost.net.au>
2024-03-28 22:14:11 +1000

    Update qtile.py

--- qtile.py
+++ qtile.py
@@ -181,10 +181,11 @@ def setup_kvm_libvirt():
         subprocess.run(['newgrp', 'libvirt'], check=True)
 
         # Enable and start the virtqemud service
-        subprocess.run(['sudo', 'systemctl', 'enable', 'libvirtd.service'], check=True)
-        subprocess.run(['sudo', 'systemctl', 'start', 'libvirtd.service'], check=True)
+        subprocess.run(['sudo', 'systemctl', 'enable', 'libvirtd'], check=True)
+        subprocess.run(['sudo', 'systemctl', 'start', 'libvirtd'], check=True)
 
         print("KVM and libvirt setup successfully.")
+    
     except subprocess.CalledProcessError as e:
         print(f"Error setting up KVM and libvirt: {e}")
 
