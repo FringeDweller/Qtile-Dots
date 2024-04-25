af68f6c2a14b057125ff83ebd052c12ce462490c
Chay <chay@infohost.net.au>
2024-04-25 13:31:03 +1000

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install wine and its dependencies
-sudo pacman -S wine wine-mono wine-gecko
+sudo pacman -Sy wine wine-mono wine-gecko
 
 # Install optional dependencies for wine
 sudo pacman -S --asdeps --needed $(pacman -Si wine | sed -n '/^Opt/,/^Conf/p' | sed '$d' | sed 's/^Opt.*://g' | sed 's/^\s*//g' | tr '\n' ' ')
